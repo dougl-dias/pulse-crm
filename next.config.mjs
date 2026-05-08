23db7ded2e11f255a9e74521d47ee1e234e0d80d
Douglas Dias <douglas18.76.80@gmail.com>
2026-05-08 16:39:01 -0300

    integrated prisma/client

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: true
   },
   images: {
-    unoptimized: true,
-  },
+    unoptimized: true
+  }
 }
 
 export default nextConfig
